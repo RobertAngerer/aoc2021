c6213566323f8d658bd5ac0d429d696306decc4e
Robert Angerer <robert.angerer@outlook.com>
2022-06-25 01:07:04 +0200

    solve day1 1

--- challenges/day01.py
+++ challenges/day01.py
@@ -1,13 +1,22 @@
 inputs = []
 def solveDay01():
-    result = []
+    temp = max(inputs)
+    counter = 0
+    for item in inputs:
+        if item > temp:
+            counter += 1
+        temp = item
+    return counter
 
 
 def parseDay01Input():
     with open("../inputs/day01", 'r') as file:
-        for line, index in file:
-            stripped_line = line.strip()
-                    inputs[index] = int(stripped_line)
+        for line in file:
+            stripped_line = int(line.strip())
+            inputs.append(stripped_line)
 
+if __name__ == '__main__':
+    parseDay01Input()
+    print(solveDay01())
 
 
